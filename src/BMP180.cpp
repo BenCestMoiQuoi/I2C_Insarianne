e032bc315d9826e9650de1fd53b309fb8cc2aefb
BEN <lateigne86@gmail.com>
2024-07-07 16:25:25 +0200

    Prise en compte LORA et BMP

--- src/BMP180.cpp
+++ src/BMP180.cpp
@@ -1,146 +1,200 @@
 /*!
  * @file BMP180.cpp
  *
- * @mainpage BMP180 Sensors
- * 
- * @section Intoduction
- * 
- * This is a library for the BMP180 Barometric Pressure + Temp sensor
+ * @mainpage Adafruit BMP085 Library
+ *
+ * @section intro_sec Introduction
+ *
+ * This is a library for the Adafruit BMP085/BMP180 Barometric Pressure + Temp
+ * sensor
+ *
+ * Designed specifically to work with the Adafruit BMP085 or BMP180 Breakout
+ * ----> http://www.adafruit.com/products/391
+ * ----> http://www.adafruit.com/products/1603
  *
  * These displays use I2C to communicate, 2 pins are required to
  * interface
- * 
- * @section Author
- * 
- * Written by Corentin Roche
- * 
+ * Adafruit invests time and resources providing this open source code,
+ * please support Adafruit and open-source hardware by purchasing
+ * products from Adafruit!
+ *
+ * @section author Author
+ *
+ * Written by Limor Fried/Ladyada for Adafruit Industries.
+ * Updated by Samy Kamkar for cross-platform support.
+ *
+ * @section license License
+ *
+ * BSD license, all text above must be included in any redistribution
  */
 
 #include "I2C_Insarianne.h"
+#include <Adafruit_I2CDevice.h>
 
+BMP180::BMP180() { i2c_dev = nullptr; }
 
-#define BMP_ADDR 0x77
+bool BMP180::begin(uint8_t mode, TwoWire *wire) {
+  if (mode > BMP085_ULTRAHIGHRES)
+    mode = BMP085_ULTRAHIGHRES;
+  oversampling = mode;
 
-#define BMP180_ULTRALOWPOWER 0 //!< Ultra low power mode
-#define BMP180_STANDARD 1      //!< Standard mode
-#define BMP180_HIGHRES 2       //!< High-res mode
-#define BMP180_ULTRAHIGHRES 3  //!< Ultra high-res mode
-#define BMP180_CAL_AC1 0xAA    //!< R   Calibration data (16 bits)
-#define BMP180_CAL_AC2 0xAC    //!< R   Calibration data (16 bits)
-#define BMP180_CAL_AC3 0xAE    //!< R   Calibration data (16 bits)
-#define BMP180_CAL_AC4 0xB0    //!< R   Calibration data (16 bits)
-#define BMP180_CAL_AC5 0xB2    //!< R   Calibration data (16 bits)
-#define BMP180_CAL_AC6 0xB4    //!< R   Calibration data (16 bits)
-#define BMP180_CAL_B1 0xB6     //!< R   Calibration data (16 bits)
-#define BMP180_CAL_B2 0xB8     //!< R   Calibration data (16 bits)
-#define BMP180_CAL_MB 0xBA     //!< R   Calibration data (16 bits)
-#define BMP180_CAL_MC 0xBC     //!< R   Calibration data (16 bits)
-#define BMP180_CAL_MD 0xBE     //!< R   Calibration data (16 bits)
+  if (i2c_dev) {
+    delete i2c_dev; // remove old interface
+  }
 
-#define BMP180_CONTROL 0xF4         //!< Control register
-#define BMP180_TEMPDATA 0xF6        //!< Temperature data register
-#define BMP180_PRESSUREDATA 0xF6    //!< Pressure data register
-#define BMP180_READTEMPCMD 0x2E     //!< Read temperature control register value
-#define BMP180_READPRESSURECMD 0x34 //!< Read pressure control register value
-#define BMP_OK_REG 0xD0             //!< Read if the sensor is ready
-#define BMP_OK 0x55                 //!< Sensor ready
+  i2c_dev = new Adafruit_I2CDevice(BMP085_I2CADDR, wire);
 
+  if (!i2c_dev->begin()) {
+    return false;
+  }
 
-BMP180::BMP180() : I2C() {
-    _addr = BMP_ADDR;
+  if (read8(0xD0) != 0x55)
+    return false;
+
+  /* read calibration data */
+  ac1 = read16(BMP085_CAL_AC1);
+  ac2 = read16(BMP085_CAL_AC2);
+  ac3 = read16(BMP085_CAL_AC3);
+  ac4 = read16(BMP085_CAL_AC4);
+  ac5 = read16(BMP085_CAL_AC5);
+  ac6 = read16(BMP085_CAL_AC6);
+
+  b1 = read16(BMP085_CAL_B1);
+  b2 = read16(BMP085_CAL_B2);
+
+  mb = read16(BMP085_CAL_MB);
+  mc = read16(BMP085_CAL_MC);
+  md = read16(BMP085_CAL_MD);
+
+  return true;
 }
 
-bool BMP180::begin() {
-    oversampling = BMP180_ULTRAHIGHRES;
-    
-    if (read8(BMP_OK_REG) != BMP_OK)
-        return false;
-
-    /* read calibration data */
-    ac1 = read16(BMP180_CAL_AC1);
-    ac2 = read16(BMP180_CAL_AC2);
-    ac3 = read16(BMP180_CAL_AC3);
-    ac4 = read16(BMP180_CAL_AC4);
-    ac5 = read16(BMP180_CAL_AC5);
-    ac6 = read16(BMP180_CAL_AC6);
-
-    b1 = read16(BMP180_CAL_B1);
-    b2 = read16(BMP180_CAL_B2);
-
-    mb = read16(BMP180_CAL_MB);
-    mc = read16(BMP180_CAL_MC);
-    md = read16(BMP180_CAL_MD);
-
-    return true;
+int32_t BMP180::computeB5(int32_t UT) {
+  int32_t X1 = (UT - (int32_t)ac6) * ((int32_t)ac5) >> 15;
+  int32_t X2 = ((int32_t)mc << 11) / (X1 + (int32_t)md);
+  return X1 + X2;
 }
 
-void BMP180::read_sensor(void) {
-    Temperature();
-    Pressure();
-    Altitude();
+uint16_t BMP180::readRawTemperature(void) {
+  write8(BMP085_CONTROL, BMP085_READTEMPCMD);
+  delay(5);
+  return read16(BMP085_TEMPDATA);
 }
 
-void BMP180::Set_SLP(float altitude_base) {
-    Temperature();
-    Pressure();
+uint32_t BMP180::readRawPressure(void) {
+  uint32_t raw;
 
-    sealevelpressure = pressure / pow(1.0 - altitude_base / 44330, 5.255);
+  write8(BMP085_CONTROL, BMP085_READPRESSURECMD + (oversampling << 6));
+
+  if (oversampling == BMP085_ULTRALOWPOWER)
+    delay(5);
+  else if (oversampling == BMP085_STANDARD)
+    delay(8);
+  else if (oversampling == BMP085_HIGHRES)
+    delay(14);
+  else
+    delay(26);
+
+  raw = read16(BMP085_PRESSUREDATA);
+
+  raw <<= 8;
+  raw |= read8(BMP085_PRESSUREDATA + 2);
+  raw >>= (8 - oversampling);
+
+  return raw;
 }
 
-void BMP180::Temperature(void) {
-    //write(BMP180_READTEMPCMD, 1, true, BMP180_CONTROL, 1);
-    UT = read16(BMP180_TEMPDATA);
-    
-    x1 = (UT - (int32_t)ac6) * ((int32_t)ac5) >> 15;
-    x2 = ((int32_t)mc << 11) / (x1 + (int32_t)md);
-    
-    temperature = (x1 + x2 + 8) >> 4;
-    temperature /= 10;
+void  BMP180::read_sensor(void) {
+  readPressure();
+  readTemperature();
+  readAltitude();
 }
 
-void BMP180::Pressure(void) {
-    write(BMP180_READPRESSURECMD + (oversampling << 6), BMP180_CONTROL);
-    
-    if (oversampling == BMP180_ULTRALOWPOWER)
-        delay(5);
-    else if (oversampling == BMP180_STANDARD)
-        delay(8);
-    else if (oversampling == BMP180_HIGHRES)
-        delay(14);
-    else
-        delay(26);
+void BMP180::readPressure(void) {
+  int32_t UT, UP, B3, B5, B6, X1, X2, X3, p;
+  uint32_t B4, B7;
 
-    c1 = read16(BMP180_PRESSUREDATA);
+  UT = readRawTemperature();
+  UP = readRawPressure();
 
-    c1 <<= 8;
-    c1 |= read8(BMP180_PRESSUREDATA + 2);
-    c1 >>= (8 - oversampling); 
+  B5 = computeB5(UT);
+
+  // do pressure calcs
+  B6 = B5 - 4000;
+  X1 = ((int32_t)b2 * ((B6 * B6) >> 12)) >> 11;
+  X2 = ((int32_t)ac2 * B6) >> 11;
+  X3 = X1 + X2;
+  B3 = ((((int32_t)ac1 * 4 + X3) << oversampling) + 2) / 4;
+
+  X1 = ((int32_t)ac3 * B6) >> 13;
+  X2 = ((int32_t)b1 * ((B6 * B6) >> 12)) >> 16;
+  X3 = ((X1 + X2) + 2) >> 2;
+  B4 = ((uint32_t)ac4 * (uint32_t)(X3 + 32768)) >> 15;
+  B7 = ((uint32_t)UP - B3) * (uint32_t)(50000UL >> oversampling);
 
 
-    c6 = c5 - 4000;
-    x1 = ((int32_t)b2 * ((c6 * c6) >> 12)) >> 11;
-    x2 = ((int32_t)ac2 * c6) >> 11;
-    x3 = x1 + x2;
-    c3 = ((((int32_t)ac1 * 4 + x3) << oversampling) + 2) / 4;
+  if (B7 < 0x80000000) {
+    p = (B7 * 2) / B4;
+  } else {
+    p = (B7 / B4) * 2;
+  }
+  X1 = (p >> 8) * (p >> 8);
+  X1 = (X1 * 3038) >> 16;
+  X2 = (-7357 * p) >> 16;
 
-    x1 = ((int32_t)ac3 * c6) >> 13;
-    x2 = ((int32_t)b1 * ((c6 * c6) >> 12)) >> 16;
-    x3 = ((x1 + x2) + 2) >> 2;
-    c4 = ((uint32_t)ac4 * (uint32_t)(x3 + 32768)) >> 15;
-    c7 = ((uint32_t)UP - c3) * (uint32_t)(50000UL >> oversampling);
-
-    if (c7 < 0x80000000) {
-        x3 = (c7 * 2) / c4;
-    } else {
-        x3 = (c7 / c4) * 2;
-    }
-    x1 = (x3 >> 8) * (x3 >> 8);
-    x1 = (x1 * 3038) >> 16;
-    x2 = (-7357 * x3) >> 16;
-
-    pressure = x3 + ((x1 + x2 + (int32_t)3791) >> 4);
+  p = p + ((X1 + X2 + (int32_t)3791) >> 4);
+  pressure = p;
 }
 
-void BMP180::Altitude(void) {
-    altitude = 44330 * (1.0 - pow(pressure / sealevelpressure, 0.1903));
+void BMP180::setSealevelPressure(float altitude_meters) {
+  readPressure();
+  sealevelpressure = (int32_t)(pressure / pow(1.0 - altitude_meters / 44330, 5.255));
+}
+
+void BMP180::readTemperature(void) {
+  int32_t UT, B5; // following ds convention
+  float temp;
+
+  UT = readRawTemperature();
+
+  B5 = computeB5(UT);
+  temp = (B5 + 8) >> 4;
+  temp /= 10;
+
+  temperature = temp;
+}
+
+void BMP180::readAltitude(void) {
+  altitude = 44330 * (1.0 - pow(pressure / sealevelpressure, 0.1903));
+}
+
+/*********************************************************************/
+
+uint8_t BMP180::read8(uint8_t a) {
+  uint8_t ret;
+
+  // send 1 byte, reset i2c, read 1 byte
+  i2c_dev->write_then_read(&a, 1, &ret, 1, true);
+
+  return ret;
+}
+
+uint16_t BMP180::read16(uint8_t a) {
+  uint8_t retbuf[2];
+  uint16_t ret;
+
+  // send 1 byte, reset i2c, read 2 bytes
+  // we could typecast uint16_t as uint8_t array but would need to ensure proper
+  // endianness
+  i2c_dev->write_then_read(&a, 1, retbuf, 2, true);
+
+  // write_then_read uses uint8_t array
+  ret = retbuf[1] | (retbuf[0] << 8);
+
+  return ret;
+}
+
+void BMP180::write8(uint8_t a, uint8_t d) {
+  // send d prefixed with a (a d [stop])
+  i2c_dev->write(&d, 1, true, &a, 1);
 }
